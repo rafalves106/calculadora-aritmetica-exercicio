d1a58878ee660c97834bbf0ddebdd1abdd2d5e17
rafalves106 <rafalves106@icloud.com>
2024-10-02 22:57:13 -0300

    projeto final

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,86 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, computed } from 'vue';
+
+
+const estado = reactive ({
+  numero1: 0,
+  numero2: 0,
+  operacao: 'adicao',
+})
+
+const resultado = computed (() => {
+  const n1 = parseFloat(estado.numero1);
+  const n2 = parseFloat(estado.numero2);
+
+  switch (estado.operacao) {
+    case 'adicao':
+      return n1 + n2;
+    case 'subtracao':
+      return n1 - n2;
+    case 'multiplicacao':
+      return n1 * n2;
+    case 'divisao':
+      return n1 / n2;
+    default:
+      return 0;
+  }
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="campos">
+      <div class="campos-pnumero">
+        <input type="number" v-model="estado.numero1" placeholder="Adicione o número">
+      </div>
+      <div class="campos-selecao">
+        <select v-model="estado.operacao">
+          <option value="multiplicacao">Multiplicar ×</option>
+          <option value="divisao">Dividir ÷</option>
+          <option value="adicao">Somar +</option>
+          <option value="subtracao">Subtrair -</option>
+        </select>
+      </div>
+      <div class="campos-snumero">
+        <input type="number" v-model="estado.numero2" placeholder="Adicione o número">
+      </div>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="resultado">
+      <h4>Resultado: {{ resultado }}</h4>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
+  .container {
+    width: 100%;
+    max-width: 300px;
+    height: 95vh;
+    margin: 0 auto;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
+  .campos {
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    flex-direction: column;
+    gap: 10px;
   }
-}
+
+  .campos input, .campos select {
+    width: 100%;
+    height: 30px;
+    text-align: right;
+  }
 </style>
